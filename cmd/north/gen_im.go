35d81d24546d55b8f3af0ef766a4d0e820928ce0
loveyourstack <loveyourstack@gmail.com>
2024-11-15 13:35:36 +0100

    use db owner for gen calls

--- cmd/north/gen_im.go
+++ cmd/north/gen_im.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/loveyourstack/lys/lysgen"
+	"github.com/loveyourstack/lys/lyspgdb"
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +16,23 @@ var genImCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 
-		res, err := lysgen.InputModel(cmd.Context(), cliApp.Db, args[0], args[1])
+		defer cliApp.Db.Close()
+
+		// connect as owner (for permission to view constraints)
+		dbOwnerUser := lyspgdb.User{
+			Name:     cliApp.Config.DbOwnerUser.Name,
+			Password: cliApp.Config.DbOwnerUser.Password,
+		}
+
+		ctx := context.Background()
+		ownerDb, err := lyspgdb.GetPool(ctx, cliApp.Config.Db, dbOwnerUser)
+		if err != nil {
+			cliApp.ErrorLog.Error("lyspgdb.GetPool (db owner) failed: " + err.Error())
+			os.Exit(1)
+		}
+		defer ownerDb.Close()
+
+		res, err := lysgen.InputModel(cmd.Context(), ownerDb, args[0], args[1])
 		if err != nil {
 			cliApp.ErrorLog.Error("lysgen.InputModel failed: " + err.Error())
 			os.Exit(1)
