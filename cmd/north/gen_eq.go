a07da17bfdeb5334748fac511c72625ca2c13c78
loveyourstack <loveyourstack@gmail.com>
2024-11-19 15:47:34 +0100

    add lys gen Equal func

---
+++ cmd/north/gen_eq.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/loveyourstack/lys/lysgen"
+	"github.com/loveyourstack/lys/lyspgdb"
+	"github.com/spf13/cobra"
+)
+
+var genEqCmd = &cobra.Command{
+	Use:   "eq",
+	Short: "generates Go Equal func from the supplied schema + table",
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		defer cliApp.Db.Close()
+
+		// connect as owner (for permission to view constraints)
+		dbOwnerUser := lyspgdb.User{
+			Name:     cliApp.Config.DbOwnerUser.Name,
+			Password: cliApp.Config.DbOwnerUser.Password,
+		}
+
+		ctx := context.Background()
+		ownerDb, err := lyspgdb.GetPool(ctx, cliApp.Config.Db, dbOwnerUser)
+		if err != nil {
+			cliApp.ErrorLog.Error("lyspgdb.GetPool (db owner) failed: " + err.Error())
+			os.Exit(1)
+		}
+		defer ownerDb.Close()
+
+		res, err := lysgen.Equal(cmd.Context(), ownerDb, args[0], args[1])
+		if err != nil {
+			cliApp.ErrorLog.Error("lysgen.Equal failed: " + err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println(res)
+	},
+}
+
+func init() {
+	genCmd.AddCommand(genEqCmd)
+}
--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/gorilla/mux v1.8.1
 	github.com/jackc/pgx/v5 v5.7.1
 	github.com/loveyourstack/connectors v0.1.7
-	github.com/loveyourstack/lys v0.1.30
+	github.com/loveyourstack/lys v0.1.31
 	github.com/spf13/cobra v1.8.0
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842
 )
