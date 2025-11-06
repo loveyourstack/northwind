45b7548d54e013984fc989845f58caa0c3dc05dd
loveyourstack <loveyourstack@gmail.com>
2025-11-06 13:44:31 +0100

    fix blob req error handling

--- frontend/backoffice-ui/src/api/index.ts
+++ frontend/backoffice-ui/src/api/index.ts
@@ -15,6 +15,32 @@ ax.interceptors.response.use(
   (error) => {
     //console.log(error.toJSON())
 
+    // special handling for blob requests (file downloads: see file.ts) errors in order to get err_description
+    // from https://github.com/axios/axios/issues/815
+    if (error.request.responseType === 'blob') {
+      return new Promise((resolve, reject) => {
+          let reader = new FileReader();
+          reader.onload = () => {
+              error.response.data = JSON.parse(String(reader.result))
+
+              appStore.apiErr = { 
+                method: error.config.method.toUpperCase(), 
+                url: error.config.url, 
+                errMsg: error.response.status + ' - ' + error.response.data.err_description 
+              }
+
+              resolve(Promise.reject(error))
+          }
+
+          reader.onerror = () => {
+              reject(error)
+          }
+          reader.readAsText(error.response.data)
+      })
+    }
+
+    // regular requests
+
     appStore.apiErr = { 
       method: error.config.method.toUpperCase(), 
       url: error.config.url, 
